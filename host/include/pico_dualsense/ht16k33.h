2d310992231a9724fd73ba78003f5ee16490571a
sonnny <sonnybalut@yahoo.com>
2023-08-24 08:46:38 -0700

    Add files via upload

---
+++ host/include/pico_dualsense/ht16k33.h
@@ -0,0 +1,9 @@
+
+#ifndef __HT16K33_H__
+#define __HT16K33_H__
+#include <stdint.h>
+
+void ht16k33_init();
+void ht16k33_display(uint8_t x, uint8_t y);
+
+#endif
---
+++ host/include/pico_dualsense/ws2812.h
@@ -0,0 +1,8 @@
+#ifndef __WS2812_H__
+#define __WS2812_H__
+#include <stdint.h>
+
+void ws2812_init();
+void ws2812_update(uint8_t index);
+
+#endif
